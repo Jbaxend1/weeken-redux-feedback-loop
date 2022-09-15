9f8951c15c96222bb0af66811e02ae434298df88
Jonathon Baxendell <j.bax626@gmail.com>
2022-09-15 17:52:04 -0500

    reducer setup and feeling reducer created

--- src/components/Feeling/Feeling.jsx
+++ src/components/Feeling/Feeling.jsx
@@ -2,6 +2,9 @@ const Feeling = () => {
     return (
         <>
             <h3>How Are You Feeling Today?</h3>
+            <h5>0 being bad and 5 being Great!</h5>
+            <input type='number' placeholder='0' required='required' min={0} max={5}></input>
+            <button>Next</button>
         </>
     )
 }
