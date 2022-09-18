45d02daf0121838a01756ed735f676eb7074c3fc
Jonathon Baxendell <j.bax626@gmail.com>
2022-09-18 10:27:36 -0500

    feeling component input validation

--- src/components/Feeling/Feeling.jsx
+++ src/components/Feeling/Feeling.jsx
@@ -1,5 +1,6 @@
 import { useHistory } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
+import { useState } from "react";
 
 const Feeling = () => {
 
@@ -7,10 +8,22 @@ const Feeling = () => {
 
     const feeling = useSelector( store => store.feeling);
 
+    const [ isValid, setIsvalid ] = useState(false);
+
     const dispatch = useDispatch();
 
     const handleChange = (event) => {
         dispatch({ type: 'SET_FEELING', payload: event.target.value });
+        setIsvalid(true);
+    }
+
+    const handleValid = (e) => {
+        if (e.target.value === 'true') {
+            history.push('/understand');
+        } else {
+            alert('Score Inputs Must Be Valid!')
+        }
+        
     }
 
     return (
@@ -18,7 +31,7 @@ const Feeling = () => {
             <h3>How Are You Feeling Today?</h3>
             <h5>0 being bad and 5 being Great!</h5>
             <input onChange={handleChange} type='number' placeholder='0' required='required' min={0} max={5}></input>
-            <button onClick={() => history.push('/understand')}>Next</button>
+            <button value={isValid} onClick={handleValid}>Next</button>
         </>
     )
 }
